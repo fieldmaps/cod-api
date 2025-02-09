4dcd7a65183f012c2d92d263cfeec240fd7a0452
Max Malynowsky <github@maxmalynowsky.com>
2025-02-09 21:35:16 +0100

    use slim docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,4 @@
-FROM ghcr.io/osgeo/gdal:ubuntu-full-3.10.1
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends \
-    python3-pip python3-venv rclone \
-    && rm -rf /var/lib/apt/lists/*
-
-RUN python3 -m venv /opt/venv
-ENV PATH="/opt/venv/bin:$PATH"
+FROM python:3.13-slim
 
 WORKDIR /usr/src/app
 
--- app/routers/features.py
+++ app/routers/features.py
@@ -1,7 +1,4 @@
-from asyncio import create_subprocess_exec
 from os import getenv
-from pathlib import Path
-from tempfile import TemporaryDirectory
 from uuid import uuid4
 
 # from typing import Annotated
@@ -10,31 +7,11 @@ from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import RedirectResponse
 from httpx import AsyncClient, codes
 
+GDAL_URL = getenv("GDAL_URL", "")
 S3_ASSETS_URL = getenv("S3_ASSETS_URL", "")
 S3_CACHE_URL = getenv("S3_CACHE_URL", "")
-S3_ASSETS_BUCKET = getenv("S3_ASSETS_BUCKET", "")
-S3_CACHE_BUCKET = getenv("S3_CACHE_BUCKET", "")
-S3_CHUNK_SIZE = getenv("S3_CHUNK_SIZE", "")
 
 router = APIRouter()
-
-
-def get_recommended_options(local_format: str) -> list[str]:
-    """Get recommended options for output formats."""
-    match local_format:
-        case "shp.zip":
-            return ["-lco", "ENCODING=UTF-8"]
-        case _:
-            return []
-
-
-def get_local_format(local_format: str) -> str:
-    """Remove .zip from formats outputing to directory and add .zip to shapefiles."""
-    if local_format == "gdb.zip":
-        return local_format.rstrip(".zip")
-    if local_format == "shp":
-        return f"{local_format}.zip"
-    return local_format
 
 
 def get_remote_format(remote_format: str) -> str:
@@ -68,66 +45,25 @@ async def features(
     processing_level = processing_level.lower()
     layer = f"{iso3}_adm{admin_level}".lower()
     assets_url = f"{S3_ASSETS_URL}/level-{processing_level}/{layer}.parquet"
-    assets_bucket = f"{S3_ASSETS_BUCKET}/level-{processing_level}/{layer}.parquet"
     if f == "parquet":
         return assets_url
-    local_format = get_local_format(f)
     remote_format = get_remote_format(f)
     cache_url = f"{S3_CACHE_URL}/level-{processing_level}/{layer}.{remote_format}"
-    cache_bucket = f"{S3_CACHE_BUCKET}/level-{processing_level}/{layer}.{remote_format}"
     async with AsyncClient() as client:
-        response = await client.head(cache_url + f"?v={uuid4()}")
-    if response.status_code == codes.OK:
+        r1 = await client.head(cache_url + f"?v={uuid4()}")
+    if r1.status_code == codes.OK:
         return cache_url
-    recommended_options = get_recommended_options(local_format)
     # lco_options = [("-lco", x) for x in lco] if lco is not None else []
     # simplify_options = ["-simplify", simplify] if simplify is not None else []
-    with TemporaryDirectory() as tmp:
-        input_path = Path(tmp) / f"{layer}.parquet"
-        output_path = Path(tmp) / f"{layer}.{local_format}"
-        rclone_download = await create_subprocess_exec(
-            "rclone",
-            "copyto",
-            *["--s3-chunk-size", S3_CHUNK_SIZE],
-            assets_bucket,
-            input_path,
+    async with AsyncClient() as client:
+        r2 = await client.get(
+            f"{GDAL_URL}/ogr2ogr/{processing_level}/{iso3}/{admin_level}?f={f}",
         )
-        await rclone_download.wait()
-        ogr2ogr = await create_subprocess_exec(
-            "ogr2ogr",
-            "-overwrite",
-            *["--config", "GDAL_NUM_THREADS", "ALL_CPUS"],
-            *["--config", "OGR_GEOJSON_MAX_OBJ_SIZE", "0"],
-            *["--config", "OGR_ORGANIZE_POLYGONS", "ONLY_CCW"],
-            *["-nln", layer],
-            # *simplify_options,
-            # *[x for y in lco_options for x in y],
-            *recommended_options,
-            output_path,
-            assets_url,
+    if r2.status_code == codes.OK:
+        return cache_url + f"?v={uuid4()}"
+    if r2.status_code >= codes.BAD_REQUEST:
+        raise HTTPException(
+            status_code=r2.status_code,
+            detail=r2.content,
         )
-        await ogr2ogr.wait()
-        if output_path.stat().st_size == 0:
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail="Unprocessable Content",
-            )
-        if output_path.is_dir():
-            output_zip = output_path.with_suffix(f".{f}.zip")
-            sozip = await create_subprocess_exec(
-                "sozip",
-                "-r",
-                output_zip,
-                output_path,
-            )
-            await sozip.wait()
-            output_path = output_zip
-        rclone_upload = await create_subprocess_exec(
-            "rclone",
-            "copyto",
-            *["--s3-chunk-size", S3_CHUNK_SIZE],
-            output_path,
-            cache_bucket,
-        )
-        await rclone_upload.wait()
-    return cache_url + f"?v={uuid4()}"
+    return ""
