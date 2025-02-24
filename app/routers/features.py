50569912762d1fb5501bf5c88e6890f94218a1c8
Max Malynowsky <github@maxmalynowsky.com>
2025-02-24 19:13:29 +0100

    add all endpoint

--- app/routers/features.py
+++ app/routers/features.py
@@ -20,6 +20,46 @@ def get_remote_format(remote_format: str) -> str:
 
 
 @router.get(
+    "/features/{processing_level}/{iso3}",
+    description="Get vector in any GDAL/OGR supported format",
+    tags=["vectors"],
+    response_class=RedirectResponse,
+    status_code=status.HTTP_308_PERMANENT_REDIRECT,
+)
+async def features_all(
+    processing_level: str,
+    iso3: str,
+    f: str = "geojson",
+) -> str:
+    """Convert features to other file format."""
+    f = f.lower().lstrip(".")
+    processing_level = processing_level.lower()
+    layer = f"{iso3}".lower()
+    assets_url = f"{S3_ASSETS_URL}/level-{processing_level}/{layer}.parquet"
+    if f == "parquet":
+        return assets_url
+    remote_format = get_remote_format(f)
+    cache_url = f"{S3_CACHE_URL}/level-{processing_level}/{layer}.{remote_format}"
+    async with AsyncClient() as client:
+        r1 = await client.head(cache_url + f"?v={uuid4()}")
+    if r1.status_code == codes.OK:
+        return cache_url
+    async with AsyncClient() as client:
+        r2 = await client.get(
+            f"{GDAL_URL}/ogr2ogr/{processing_level}/{iso3}?f={f}",
+            timeout=300,
+        )
+    if r2.status_code == codes.OK:
+        return cache_url
+    if r2.status_code >= codes.BAD_REQUEST:
+        raise HTTPException(
+            status_code=r2.status_code,
+            detail=r2.content,
+        )
+    return ""
+
+
+@router.get(
     "/features/{processing_level}/{iso3}/{admin_level}",
     description="Get vector in any GDAL/OGR supported format",
     tags=["vectors"],
